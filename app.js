86089a5f81cacce2e7bfb814250764a0e8c5a2d2
Bryan Stoner <bryan@fastspot.com>
2018-09-20 17:32:27 -0400

    connect to mongodb

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'myproject';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
